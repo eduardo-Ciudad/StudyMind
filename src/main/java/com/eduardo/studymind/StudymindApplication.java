35d09ed6d34ccb51d779ae6fc79789f00b02c3f5
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-04-23 19:54:23 -0300

    primeiro commit

---
+++ src/main/java/com/eduardo/studymind/StudymindApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.studymind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudymindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudymindApplication.class, args);
+	}
+
+}
