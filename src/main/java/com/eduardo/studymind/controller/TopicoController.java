1a95328e908cedb4fa54364a62a26dc063af1d27
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-04-28 19:02:45 -0300

    terminei o crud de Topico

--- src/main/java/com/eduardo/studymind/controller/TopicoController.java
+++ src/main/java/com/eduardo/studymind/controller/TopicoController.java
@@ -2,18 +2,20 @@ package com.eduardo.studymind.controller;
 
 
 import com.eduardo.studymind.domain.materia.MateriaRepository;
+import com.eduardo.studymind.dto.input.topico.DadosAtualizacaoTopico;
 import com.eduardo.studymind.dto.input.topico.DadosCadastroTopico;
 import com.eduardo.studymind.dto.output.topico.DadosDetalhamentoTopico;
+import com.eduardo.studymind.dto.output.topico.DadosListagemTopico;
 import com.eduardo.studymind.service.TopicoService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/topicos")
@@ -26,5 +28,26 @@ public class TopicoController {
         var topico = topicoService.cadastrarTopico(dados);
         var uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.id()).toUri();
         return ResponseEntity.created(uri).body(topico);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DadosListagemTopico>> listar (@RequestParam(required = false) Long materiaID){
+        return ResponseEntity.ok(topicoService.listarTopicos(materiaID));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DadosDetalhamentoTopico> detalhar (@PathVariable Long id){
+        return ResponseEntity.ok(topicoService.buscarPorId(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DadosDetalhamentoTopico> atualizar (@PathVariable Long id, @RequestBody @Valid DadosAtualizacaoTopico dados){
+        return ResponseEntity.ok(topicoService.atualizar(id, dados));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> desativar (@PathVariable Long id){
+        topicoService.desativar(id);
+        return ResponseEntity.noContent().build();
     }
 }
