3795c9e09d55b295251f4e7ff061a92ea3c59057
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-04-30 19:49:20 -0300

    criei a class User details

--- src/main/java/com/eduardo/studymind/domain/usuario/Usuario.java
+++ src/main/java/com/eduardo/studymind/domain/usuario/Usuario.java
@@ -2,9 +2,14 @@ package com.eduardo.studymind.domain.usuario;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "usuarios")
@@ -13,7 +18,29 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class Usuario {
+public class Usuario  implements UserDetails {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getPassword() { return senha; }
+
+    @Override
+    public String getUsername() { return email; }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return ativo; }
 
     @Id// definiu como chave primaria
     @GeneratedValue(strategy = GenerationType.IDENTITY)
