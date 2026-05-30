82697d5d7a003a47cf9198078325dc096279f7b1
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-05-29 23:13:09 -0300

    fix: mudei verifica para verificar estava dando divergencia

--- src/main/java/com/eduardo/studymind/service/EmailService.java
+++ src/main/java/com/eduardo/studymind/service/EmailService.java
@@ -28,7 +28,7 @@ public class EmailService {
     @Async
     public void enviarEmailVerificacao(Usuario usuario, String token){
         String assunto = "Verifique seu email - StudyMind";
-        String url = urlSite + "/auth/verifica?token=" + token;
+        String url = urlSite + "/auth/verificar?token=" + token;
         String conteudo = gerarConteudo(usuario.getNome(), url);
         enviarEmail(usuario.getEmail(), assunto, conteudo);
     }
