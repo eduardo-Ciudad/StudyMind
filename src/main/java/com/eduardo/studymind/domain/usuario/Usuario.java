4ab338396f968924219f48ba0a5b7749e4086f9a
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-06-30 21:11:31 -0300

    fix: troque o enum de ROLE_ADMIN para ADMIN

--- src/main/java/com/eduardo/studymind/domain/usuario/Usuario.java
+++ src/main/java/com/eduardo/studymind/domain/usuario/Usuario.java
@@ -21,7 +21,7 @@ import java.util.List;
 public class Usuario  implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+        return List.of(new SimpleGrantedAuthority( role.name()));
     }
 
     @Override
